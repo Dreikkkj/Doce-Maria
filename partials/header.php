733352882d5029fc26cf55247566c90f2ee82f83
Dreikkj <andreiluna71@gmail.com>
2026-08-31 17:01:49 -0300

    estilizando o carrinho

---
+++ partials/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+            <ul class="menu-superior">
+                <h1 class="logo_menu">Doce Maria</h1> <!-- Vamos mudar depois para colocar a foto da logo-->
+                <li>
+                    <a class="btnentrar" href="./login.php">Entrar</a>
+                </li>
+                <li>
+                    <a class="btncad" href="./index.php">Cadastre-se</a>
+                </li>
+            </ul>
+        </nav>
+</header>
