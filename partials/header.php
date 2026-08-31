ddf22971d0f22c45e8e8d263e9abb3737fac7919
BEATRIZ FARIAS SILVA <25254242@educ123.sp.senai.br>
2026-08-31 14:46:59 -0300

    eu

---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+            <ul class="menu-superior">
+                <h1 class="logo_menu">Beatriz Farias</h1>
+
+                <li>
+                    <a class="dropbtn" href="./login.php">Login<img src="./imagem/cadeado.png" width="25px" alt="Cadeado"></a>
+                </li>
+                <li>
+                    <a class="dropbtn" href="./index.php">Home<img src="./imagem/home.png" width="25px" alt="Home"></a>
+                </li>
+            </ul>
+        </nav>
+</header>
