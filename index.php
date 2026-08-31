51b7b582fae5b6bcbec045b6494b861c40d5fb25
BEATRIZ FARIAS SILVA <25254242@educ123.sp.senai.br>
2026-08-31 17:06:00 -0300

    add

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 <html lang="en">
 
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Qwitcher+Grypen:wght@700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -12,11 +18,23 @@
     <?php
     require_once "./partials/header.php"
         ?>
-    <div>
-        <img src="./imagens/homeft.png" class="fthome">
-        <h1>Felicidade em cada pedaço</h1>
+    <main>
+        <div class="container-foto">
+            <img src="./imagens/homeft.png" class="fthome">
+            <h1 class="texto-foto">Felicidade em cada pedaço</h1>
+            <button type="button" class="btn-foto">Descubra sua Felicidade</button>
+        </div>
+        <div class="bck-pink"></div>
+    </main>
 
-    </div>
+    <section>
+        <h3 class="txtseller">Nossos Best <strong class="brw">Sellers</strong></h3>
+        <div class="box-seller">
+            <div>
+
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
